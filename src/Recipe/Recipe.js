9479e73ab0957fc77a2518cf4a2a496bd9733c92
yunseong9999 <dbsdltjd3701@naver.com>
2021-05-14 21:58:00 +0900

    make Recipe, include Header

---
+++ src/Recipe/Recipe.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Recipe.css";
+import Header from "../Home/Header";
+
+const Recipe = () => {
+  return (
+    <div className="Recipe">
+      <Header />
+    </div>
+  );
+};
+export default Recipe;
