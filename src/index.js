5f3d7193725093e436a45dd887bd6f7af7adaba7
yunseong9999 <dbsdltjd3701@naver.com>
2021-05-14 21:59:18 +0900

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Home from "./Home/Home.js";
+import Recipe from "./Recipe/Recipe.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Home /> */}
+    <Recipe />
   </React.StrictMode>,
   document.getElementById("root")
 );
