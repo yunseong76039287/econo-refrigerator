ffd5841dd477899383e0cc674489b6febca85bb8
yunseong9999 <dbsdltjd3701@naver.com>
2021-07-03 14:53:41 +0900

    add layout introduction

--- src/Recipe/Recipe.js
+++ src/Recipe/Recipe.js
@@ -3,7 +3,7 @@ import "./Recipe.css";
 
 // description을 컴포넌트로 받아올 수 있나?
 
-const Recipe = ({ name, imageSrc, description, count }) => {
+const Recipe = ({ name, imageSrc, introduction, description, count }) => {
   const str =
     "레시피의 설명이 들어갈 곳<br><br/> 1. 팬을 달굽니다.<br><br/> 2. 만든 양념을 약간 태워서 불맛을 냅니다.<br><br/> 3. 고기를 같이 볶습니다.<br><br/> 4. 고기를 볶은 후에 채소를 넣어줍니다.<br><br/> 5. 그렇게 합니다.<br><br/>";
   const CountUp = () => {
@@ -21,6 +21,11 @@ const Recipe = ({ name, imageSrc, description, count }) => {
         src="https://recipe1.ezmember.co.kr/cache/recipe/2015/05/27/38013d1dfd8fa46a871b9cda074b26341.jpg"
         alt="대체 텍스트가 들어갈 곳"
       ></img>
+      <h2>레시피 소개</h2>
+      <div>
+        introduction 이 들어갈 자리입니다. recipebox에 표시되는 레시피 간단
+        설명과 같음!
+      </div>
       <h2>필요한 재료!</h2>
       <div className="recipe-ingredient">양파 간장 상추 돼지고기 100g </div>
       <h2>레시피 설명</h2>
