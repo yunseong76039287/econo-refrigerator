75a76b0a7ddd70b94e82d45113e437d704fb0cda
yunseong9999 <dbsdltjd3701@naver.com>
2021-07-03 14:51:51 +0900

    improve css style

--- src/Home/Ingredient.js
+++ src/Home/Ingredient.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Ingredient.css";
 
-
-const Ingredient = ({ selectedIngredients, setSelectedIngredients}) => {
+const Ingredient = ({ selectedIngredients, setSelectedIngredients }) => {
   const selectIngredient = (event, selected) => {
     let newSelectedIngredients = [];
 
@@ -12,52 +11,54 @@ const Ingredient = ({ selectedIngredients, setSelectedIngredients}) => {
     });
     newSelectedIngredients.push(selected);
     setSelectedIngredients(newSelectedIngredients);
-    
+
     localStorage.setItem("refrigerator", JSON.stringify(selectedIngredients));
   };
 
   let ingredientArray = [
     {
-      "id" : 1,
-      "name" : "양파",
-      "imageUrl" : "/images/onion.jpg"
+      id: 1,
+      name: "양파",
+      imageUrl: "/images/onion.jpg",
     },
     {
-      "id" : 2,
-      "name" : "대파",
-      "imageUrl" : "/images/greenonion.jpg"
+      id: 2,
+      name: "대파",
+      imageUrl: "/images/greenonion.jpg",
     },
     {
-      "id" : 3,
-      "name" : "간장",
-      "imageUrl" : "/images/soysauce.jpg"
+      id: 3,
+      name: "간장",
+      imageUrl: "/images/soysauce.jpg",
     },
     {
-      "id" : 4,
-      "name" : "된장",
-      "imageUrl" : "/images/Doenjang.jpg"
+      id: 4,
+      name: "된장",
+      imageUrl: "/images/Doenjang.jpg",
     },
     {
-      "id" : 5,
-      "name" : "고추장",
-      "imageUrl" : "/images/gochujang.jpg"
+      id: 5,
+      name: "고추장",
+      imageUrl: "/images/gochujang.jpg",
     },
   ];
-  function showIngredient({name, imageUrl}){
-    return(
+  function showIngredient({ name, imageUrl }) {
+    return (
       <div className="ingredient-container-item">
         <div className="ingredient-header">
           <span>{name}</span>
           <button onClick={() => selectIngredient(this, name)}>추가</button>
         </div>
-        <img src={imageUrl}></img>
+        <div style="text-align:">
+          <img src={imageUrl}></img>
+        </div>
       </div>
     );
-  };
+  }
 
   return (
     <div className="ingredient-container">
-    {ingredientArray.map(showIngredient)}
+      {ingredientArray.map(showIngredient)}
     </div>
   );
 };
