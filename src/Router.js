9eb94e6339d98a4024b939ee125fd461a355b6ae
yunseong9999 <dbsdltjd3701@naver.com>
2021-07-20 22:20:25 +0900

    rename initialpagetemplate -> router

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,99 @@
-import React from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import InitialPageTemplate from "./InitialPageTemplate";
+import React, { useState } from "react";
+import Header from "./Home/Header";
+import Sidebar from "./Home/Sidebar";
+import "./Router.css";
 import Home from "./Home/Home";
+import Recipe from "./Recipe/Recipe.js";
 import Search from "./Search/Search";
-import Recipe from "./Recipe/Recipe";
+import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
 
-const Router = () => {
+const Router = ({ Page }) => {
+  const [refrigeratorIngredients, setRefrigeratorIngredients] = useState([]);
+
+  // 재료를 선택할 때 중복을 검사하는 함수
+  const selectIngredient = (event, id) => {
+    const searchIngredient = refrigeratorIngredients.find((element) => {
+      if (element === id) return true;
+      return false;
+    });
+    console.log("찾은 id" + searchIngredient);
+    // 중복 없음
+    if (searchIngredient === undefined) {
+      return appendIngredientHooks(this, id);
+    }
+  };
+
+  // 재료 추가 함수
+  const appendIngredientHooks = (event, id) => {
+    let newSelectedIngredients = [];
+
+    refrigeratorIngredients.forEach((ingredientId) => {
+      newSelectedIngredients.push(ingredientId);
+    });
+
+    newSelectedIngredients.push(id);
+    setRefrigeratorIngredients(newSelectedIngredients);
+
+    localStorage.setItem(
+      "refrigerator",
+      JSON.stringify(newSelectedIngredients)
+    );
+  };
+
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/recipe">
-          <Recipe />
-        </Route>
-        <Route path="/search">
-          <InitialPageTemplate Page={Search} />
-        </Route>
-        <Route exact path="/">
-          <InitialPageTemplate Page={Home} />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <div className="initial">
+        <Header />
+        <Switch>
+          <Route
+            path="/recipe"
+            component={() => {
+              return (
+                <div className="contentsbody">
+                  <Recipe />
+                </div>
+              );
+            }}
+          >
+            <Recipe />
+          </Route>
+          <Route
+            path="/search"
+            component={() => {
+              return (
+                <div className="contentsbody">
+                  <Sidebar
+                    refrigeratorIngredients={refrigeratorIngredients}
+                    selectIngredient={selectIngredient}
+                    setRefrigeratorIngredients={setRefrigeratorIngredients}
+                  />
+                  <Search />
+                </div>
+              );
+            }}
+          ></Route>
+          <Route
+            exact={true}
+            path="/"
+            component={() => {
+              return (
+                <div className="contentsbody">
+                  <Sidebar
+                    refrigeratorIngredients={refrigeratorIngredients}
+                    selectIngredient={selectIngredient}
+                    setRefrigeratorIngredients={setRefrigeratorIngredients}
+                  />
+                  <Home
+                    refrigeratorIngredients={refrigeratorIngredients}
+                    selectIngredient={selectIngredient}
+                  />
+                </div>
+              );
+            }}
+          ></Route>
+          <Redirect to="/" />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 };
