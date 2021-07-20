855ea19c00bc70f6b8f5c6312fd21f2d4cc51b20
yunseong9999 <dbsdltjd3701@naver.com>
2021-07-20 22:19:16 +0900

    refactoring data

--- src/data/ingredientData.js
+++ src/data/ingredientData.js
@@ -1,213 +1,214 @@
+// only for front
+
 const ingredientDataArray = [
   {
-    id : "test",
-    name : "Testname",
-    imageUrl : "/images/ingredientImgData"
+    id: "test",
+    name: "Testname",
+    imageUrl: "/images/ingredientImgData",
   },
   {
-    id : 1,
-    name : "밥",
-    imageUrl : "/images/ingredientImgData/rice.jpg"
+    id: 1,
+    name: "밥",
+    imageUrl: "/images/ingredientImgData/rice.jpg",
   },
   {
-    id : 2,
-    name : "감자",
-    imageUrl : "/images/ingredientImgData/potato.jpg"
+    id: 2,
+    name: "감자",
+    imageUrl: "/images/ingredientImgData/potato.jpg",
   },
   {
-    id : 3,
-    name : "고구마",
-    imageUrl : "/images/ingredientImgData/sweet_potato.jpg"
+    id: 3,
+    name: "고구마",
+    imageUrl: "/images/ingredientImgData/sweet_potato.jpg",
   },
   {
-    id : 4,
-    name : "라면사리",
-    imageUrl : "/images/ingredientImgData/ramen_noodles.jpg"
+    id: 4,
+    name: "라면사리",
+    imageUrl: "/images/ingredientImgData/ramen_noodles.jpg",
   },
   {
-    id : 5,
-    name : "당면",
-    imageUrl : "/images/ingredientImgData/glass_noodles.jpg"
+    id: 5,
+    name: "당면",
+    imageUrl: "/images/ingredientImgData/glass_noodles.jpg",
   },
   {
-    id : 6,
-    name : "가래떡",
-    imageUrl : ""
+    id: 6,
+    name: "가래떡",
+    imageUrl: "",
   },
   {
-    id : 7,
-    name : "빵가루",
-    imageUrl : ""
+    id: 7,
+    name: "빵가루",
+    imageUrl: "",
   },
   {
-    id : 8,
-    name : "전분",
-    imageUrl : ""
+    id: 8,
+    name: "전분",
+    imageUrl: "",
   },
   {
-    id : 9,
-    name : "옥수수",
-    imageUrl : "/images/ingredientImgData/corn.jpg"
+    id: 9,
+    name: "옥수수",
+    imageUrl: "/images/ingredientImgData/corn.jpg",
   },
   {
-    id : 10,
-    name : "밀가루(강력)",
-    imageUrl : ""
+    id: 10,
+    name: "밀가루(강력)",
+    imageUrl: "",
   },
   {
-    id : 11,
-    name : "밀가루(중력)",
-    imageUrl : ""
+    id: 11,
+    name: "밀가루(중력)",
+    imageUrl: "",
   },
   {
-    id : 12,
-    name : "밀가루(박력)",
-    imageUrl : ""
+    id: 12,
+    name: "밀가루(박력)",
+    imageUrl: "",
   },
 
   {
-    id : 31,
-    name : "대파",
-    imageUrl : "/images/ingredientImgData/greenonion.jpg"
+    id: 31,
+    name: "대파",
+    imageUrl: "/images/ingredientImgData/greenonion.jpg",
   },
   {
-    id : 32,
-    name : "양파",
-    imageUrl : "/images/ingredientImgData/onion.jpg"
+    id: 32,
+    name: "양파",
+    imageUrl: "/images/ingredientImgData/onion.jpg",
   },
   {
-    id : 33,
-    name : "마늘",
-    imageUrl : "/images/ingredientImgData/garlic.jpg"
+    id: 33,
+    name: "마늘",
+    imageUrl: "/images/ingredientImgData/garlic.jpg",
   },
   {
-    id : 34,
-    name : "다진마늘",
-    imageUrl : ""
+    id: 34,
+    name: "다진마늘",
+    imageUrl: "",
   },
   {
-    id : 35,
-    name : "오이",
-    imageUrl : "/images/ingredientImgData/cucumbers.jpg"
+    id: 35,
+    name: "오이",
+    imageUrl: "/images/ingredientImgData/cucumbers.jpg",
   },
   {
-    id : 36,
-    name : "청경채",
-    imageUrl : "/images/ingredientImgData/bokchoy.jpg"
+    id: 36,
+    name: "청경채",
+    imageUrl: "/images/ingredientImgData/bokchoy.jpg",
   },
   {
-    id : 37,
-    name : "부추",
-    imageUrl : "/images/ingredientImgData/chives.jpg"
+    id: 37,
+    name: "부추",
+    imageUrl: "/images/ingredientImgData/chives.jpg",
   },
 
   {
-    id : 101,
-    name : "고추장",
-    imageUrl : "/images/ingredientImgData/gochujang.jpg"
+    id: 101,
+    name: "고추장",
+    imageUrl: "/images/ingredientImgData/gochujang.jpg",
   },
   {
-    id : 102,
-    name : "된장",
-    imageUrl : "/images/ingredientImgData/doenjang.jpg"
+    id: 102,
+    name: "된장",
+    imageUrl: "/images/ingredientImgData/doenjang.jpg",
   },
   {
-    id : 103,
-    name : "간장",
-    imageUrl : "/images/ingredientImgData/soysauce.jpg"
+    id: 103,
+    name: "간장",
+    imageUrl: "/images/ingredientImgData/soysauce.jpg",
   },
   {
-    id : 104,
-    name : "초고추장",
-    imageUrl : "/images/ingredientImgData/chogochujang.jpg"
+    id: 104,
+    name: "초고추장",
+    imageUrl: "/images/ingredientImgData/chogochujang.jpg",
   },
   {
-    id : 105,
-    name : "설탕",
-    imageUrl : "/images/ingredientImgData/sugar.jpg"
+    id: 105,
+    name: "설탕",
+    imageUrl: "/images/ingredientImgData/sugar.jpg",
   },
   {
-    id : 106,
-    name : "소금",
-    imageUrl : "/images/ingredientImgData/salt.jpg"
+    id: 106,
+    name: "소금",
+    imageUrl: "/images/ingredientImgData/salt.jpg",
   },
   {
-    id : 107,
-    name : "식용유",
-    imageUrl : "/images/ingredientImgData/oil.jpg"
+    id: 107,
+    name: "식용유",
+    imageUrl: "/images/ingredientImgData/oil.jpg",
   },
   {
-    id : 108,
-    name : "미원",
-    imageUrl : "/images/ingredientImgData/msg.jpg"
+    id: 108,
+    name: "미원",
+    imageUrl: "/images/ingredientImgData/msg.jpg",
   },
   {
-    id : 109,
-    name : "식초",
-    imageUrl : "/images/ingredientImgData/vinegar.jpg"
+    id: 109,
+    name: "식초",
+    imageUrl: "/images/ingredientImgData/vinegar.jpg",
   },
   {
-    id : 110,
-    name : "맛술",
-    imageUrl : "/images/ingredientImgData/ricewine.jpg"
+    id: 110,
+    name: "맛술",
+    imageUrl: "/images/ingredientImgData/ricewine.jpg",
   },
   {
-    id : 111,
-    name : "토마토케찹",
-    imageUrl : "/images/ingredientImgData/ketchup.jpg"
+    id: 111,
+    name: "토마토케찹",
+    imageUrl: "/images/ingredientImgData/ketchup.jpg",
   },
   {
-    id : 112,
-    name : "마요네즈",
-    imageUrl : "/images/ingredientImgData/mayonnaise.jpg"
+    id: 112,
+    name: "마요네즈",
+    imageUrl: "/images/ingredientImgData/mayonnaise.jpg",
   },
   {
-    id : 113,
-    name : "참기름",
-    imageUrl : "/images/ingredientImgData/sesame_oil.jpg"
+    id: 113,
+    name: "참기름",
+    imageUrl: "/images/ingredientImgData/sesame_oil.jpg",
   },
   {
-    id : 114,
-    name : "고춧가루",
-    imageUrl : "/images/ingredientImgData/chilli_powder.jpg"
+    id: 114,
+    name: "고춧가루",
+    imageUrl: "/images/ingredientImgData/chilli_powder.jpg",
   },
   {
-    id : 115,
-    name : "멸치액젓",
-    imageUrl : "/images/ingredientImgData/anchovy_sauce.jpg"
+    id: 115,
+    name: "멸치액젓",
+    imageUrl: "/images/ingredientImgData/anchovy_sauce.jpg",
   },
   {
-    id : 116,
-    name : "후추",
-    imageUrl : "/images/ingredientImgData/pepper.jpg"
+    id: 116,
+    name: "후추",
+    imageUrl: "/images/ingredientImgData/pepper.jpg",
   },
   {
-    id : 117,
-    name : "물엿",
-    imageUrl : "/images/ingredientImgData/corn_syrup.jpg"
+    id: 117,
+    name: "물엿",
+    imageUrl: "/images/ingredientImgData/corn_syrup.jpg",
   },
   {
-    id : 118,
-    name : "새우젓",
-    imageUrl : "/images/ingredientImgData/salted_shrimp.jpg"
+    id: 118,
+    name: "새우젓",
+    imageUrl: "/images/ingredientImgData/salted_shrimp.jpg",
   },
   {
-    id : 119,
-    name : "들깨가루",
-    imageUrl : "/images/ingredientImgData/perilla_powder.jpg"
+    id: 119,
+    name: "들깨가루",
+    imageUrl: "/images/ingredientImgData/perilla_powder.jpg",
   },
   {
-    id : 120,
-    name : "카레가루",
-    imageUrl : "/images/ingredientImgData/curry_powder.jpg"
+    id: 120,
+    name: "카레가루",
+    imageUrl: "/images/ingredientImgData/curry_powder.jpg",
   },
 
   {
-    id : 161,
-    name : "계란",
-    imageUrl : "/images/ingredientImgData/egg.jpg"
+    id: 161,
+    name: "계란",
+    imageUrl: "/images/ingredientImgData/egg.jpg",
   },
-  
 ];
 
 // 재료의 id 매개변수 받고, 그 id 의 재료 이름 반환하는 함수
--- src/data/recipeData.js
+++ src/data/recipeData.js
@@ -1,383 +1,294 @@
 let soyEggFriedRice = {
-    id: 1,
-    name: "백종원 달걀볶음밥",
-    description: "불맛 가득한 달걀볶음밥! 맛없으면 이상한거쥬~",
-    imageUrl: "",
-    likeCount: 0,
-    ingredients: [
-      {
-        id: 1,
-        ingredient: "밥",
-        amount : "1공기(200g)"
-      },
-      {
-        id: 31,
-        ingredient: "대파",
-        amount : "1/2컵"
-      },
-      {
-        id:161,
-        ingredient:"계란",
-        amount:"2알"
-      },
-      {
-        id:107,
-        ingredient:"식용유",
-        amount:"4 큰술"
-      },
-      {
-        id:108,
-        ingredient:"미원(MSG)",
-        amount:"1/4 큰술"
-      },
-      {
-        id:103,
-        ingredient:"간장",
-        amount:"1/2 큰술"
-      },
-      {
-        id:106,
-        ingredient:"소금",
-        amount:"1g"
-      },
-    ],
-    steps: [
-      {
-        id: 1,
-        description: "1. 대파는 송송 썰어 준비한다.",
-        imagePath: "",
-      },
-      {
-        id: 2,
-        description: "2. 프라이팬에 식용유, 대파를 넣어 파 기름을 내고 노릇해지면 한쪽으로 몰아둔다.",
-        imagePath: "",
-      },
-      {
-        id: 3,
-        description: "3. 다른 한쪽에 달걀을 넣어 스크램블 한다.",
-        imagePath: "",
-      },
-      {
-        id: 4,
-        description: "4. 재료가 없는 쪽에 간장을 넣어 눌어붙도록 끓여주고 스크램블 한 달걀, 볶은 대파와 섞어준다.",
-        imagePath: "",
-      },
-      {
-        id: 5,
-        description: "5. 달걀, 대파가 골고루 섞이면 맛소금, 밥을 넣고 불을 끈 후 국자를 이용하여 섞어준다.",
-        imagePath: "",
-      },
-      {
-        id: 6,
-        description: "6. 밥이 골고루 섞이면 센 불에 올려 볶는다.",
-        imagePath: "",
-      },
-      {
-        id: 7,
-        description: "7. 기호에 따라 MSG를 넣어 볶은 후 완성한다.",
-        imagePath: "",
-      }
-    ],
+  name: "백종원 달걀볶음밥",
+  description: "불맛 가득한 달걀볶음밥! 맛없으면 이상한거쥬~",
+  imageUrl: "",
+  likeCount: 0,
+  ingredients: [
+    {
+      ingredient: "밥",
+    },
+    {
+      ingredient: "대파",
+    },
+    {
+      ingredient: "계란",
+    },
+    {
+      ingredient: "식용유",
+    },
+    {
+      ingredient: "미원(MSG)",
+    },
+    {
+      ingredient: "간장",
+    },
+    {
+      ingredient: "소금",
+    },
+  ],
+  steps: [
+    {
+      description: "1. 대파는 송송 썰어 준비한다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "2. 프라이팬에 식용유, 대파를 넣어 파 기름을 내고 노릇해지면 한쪽으로 몰아둔다.",
+      imagePath: "",
+    },
+    {
+      description: "3. 다른 한쪽에 달걀을 넣어 스크램블 한다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "4. 재료가 없는 쪽에 간장을 넣어 눌어붙도록 끓여주고 스크램블 한 달걀, 볶은 대파와 섞어준다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "5. 달걀, 대파가 골고루 섞이면 맛소금, 밥을 넣고 불을 끈 후 국자를 이용하여 섞어준다.",
+      imagePath: "",
+    },
+    {
+      description: "6. 밥이 골고루 섞이면 센 불에 올려 볶는다.",
+      imagePath: "",
+    },
+    {
+      description: "7. 기호에 따라 MSG를 넣어 볶은 후 완성한다.",
+      imagePath: "",
+    },
+  ],
 
-    likeCount: 0,
-    comments: [
-    ],
-  };
-  
-  let  perillaKalguksu= {
-    id: 2,
-    name: "백종원 들깨칼국수",
-    description: "들깨의 맛, 설명이 필요없죠? 이 방법으로 칼국수, 수제비, 소면, 라면 다 가능해요.",
-    imageUrl: "",
-    likeCount: 0,
-    ingredients: [
-      {
-        id: 2,
-        ingredient: "감자",
-        amount : "2/3개"
-      },
-      {
-        id: 47,
-        ingredient: "표고버섯",
-        amount : "1개"
-      },
-      {
-        id:32,
-        ingredient:"양파",
-        amount:"약 1/4개"
-      },
-      {
-        id:13,
-        ingredient:"칼국수 면",
-        amount:"150g"
-      },
-      {
-        id:34,
-        ingredient:"다진마늘",
-        amount:"(간마늘) 1/2 큰술"
-      },
-      {
-        id:115,
-        ingredient:"멸치액젓",
-        amount:"1과 1/2 큰술"
-      },
-      {
-        id:106,
-        ingredient:"소금",
-        amount:"약간 (기호에 맞게)"
-      },
-      {
-        id:116,
-        ingredient:"멸치액젓",
-        amount:"1과 1/2 큰술"
-      },
-      {
-        id:119,
-        ingredient:"들깨가루",
-        amount:"약 5클술"
-      },
-      {
-        id:121,
-        ingredient:"들기름",
-        amount:"1큰술"
-      },
-    ],
-    steps: [
-      {
-        id: 1,
-        description: "1. 감자는 0.5cm로 채 썰고, 표고버섯은 기둥을 제거한 후 0.3cm 두께로 편 썬다.",
-        imagePath: "",
-      },
-      {
-        id: 2,
-        description: "2. 양파는 0.3cm로 채 썬다.",
-        imagePath: "",
-      },
-      {
-        id: 3,
-        description: "3. 깊은 프라이팬이나 냄비에 물, 양파, 감자, 표고버섯을 넣고 끓으면 중불로 줄인다.",
-        imagePath: "",
-      },
-      {
-        id: 4,
-        description: "4. 칼국수 면을 체에 밭쳐 흐르는 물에 헹군 후 육수에 넣어 살살 저어가며 끓인다. * 칼국수 면을 미리 헹궈두면 뭉칠 수 있으니 육수에 넣기 직전에 헹군다.",
-        imagePath: "",
-      },
-      {
-        id: 5,
-        description: "5. 다시 끓어 오르면 간 마늘, 멸치액젓을 넣고 약 4분 30초 정도 끓인다. * 불 세기에 따라 물양을 조절한다. ",
-        imagePath: "",
-      },
-      {
-        id: 6,
-        description: "6. 끓이는 중간에 간을 본 후 부족한 간은 꽃소금을 이용하여 맞춘다. * 다시다를 사용 할 경우 꽃소금은 생략 가능하다",
-        imagePath: "",
-      },
-      {
-        id: 7,
-        description: "7. 칼국수 면이 익으면 들깻가루를 넣고 골고루 섞은 후 불을 끈다.",
-        imagePath: "",
-      },
-      {
-        id :8,
-        description:"8. 들기름을 넣고 섞은 후 그릇에 담아 완성한다.",
-        imagePath: "",
-      }
-    ],
+  likeCount: 0,
+  comments: [],
+};
 
-    likeCount: 0,
-    comments: [
-    ],
-  };
-  
-  let fishCakeRamen= {
-    id: 1,
-    name: "백종원 어묵듬뿍! 해물맛 라면",
-    description: "제가 만든 마라소스가 출시되었습니다ㅎㅎ CU 가면 사실 수 있는데 ㅎㅎ백종원 마라소스와 어묵 듬뿍 넣은 라면이랑 먹으면 얼마나 맛있는지 몰라요 ㅎㅎ 사라는 건 아니고요 ㅎㅎ 그냥 그렇다고요 ㅎㅎ",
-    imageUrl: "",
-    likeCount: 0,
-    ingredients: [
-      {
-        id: 4,
-        ingredient: "라면사리",
-        amount : ""
-      },
-      {
-        id: 253,
-        ingredient: "어묵",
-        amount : "(얇은사각어묵) 2장"
-      },
-      {
-        id:43,
-        ingredient:"청양고추",
-        amount:"2개"
-      },
-      {
-        id:49,
-        ingredient:"콩나물",
-        amount:"1과 1/2컵"
-      },
-      {
-        id:31,
-        ingredient:"대파",
-        amount:"약 1/4대"
-      },
-      {
-        id:103,
-        ingredient:"간장",
-        amount:"1큰술"
-      },
-      {
-        id:122,
-        ingredient:"마라소스",
-        amount:"약 2큰술(취향에 맞게)"
-      }
-    ],
-    steps: [
-      {
-        id: 1,
-        description: "1. 냄비에 물(3과 1/2컵), 다시마, 건더기스프, 콩나물을 넣고 강 불에 끓인다.",
-        imagePath: "",
-      },
-      {
-        id: 2,
-        description: "2. 사각어묵은 반으로 잘라 두께 약 0.5cm 정도로 채 썰어 준비한다.",
-        imagePath: "",
-      },
-      {
-        id: 3,
-        description: "3. 청양고추, 대파는 두께 0.3cm로 송송 썰어 준비한다.",
-        imagePath: "",
-      },
-      {
-        id: 4,
-        description: "4. 육수가 끓어 오르면 준비해둔 어묵과 라면사리, 국간장을 넣고 끓인다.",
-        imagePath: "",
-      },
-      {
-        id: 5,
-        description: "5. 면이 전체적으로 한 가닥씩 풀어지면 마라소스를 넣어 잘 섞어가며 약 1분정도 끓인다. *마라소스가 없다면 굵은고추가루를 넣고 끓여도 좋다.",
-        imagePath: "",
-      },
-      {
-        id: 6,
-        description: "6. 청양고추, 대파를 넣고 섞어 완성한다.",
-        imagePath: "",
-      }
-    ],
+let perillaKalguksu = {
+  name: "백종원 들깨칼국수",
+  description:
+    "들깨의 맛, 설명이 필요없죠? 이 방법으로 칼국수, 수제비, 소면, 라면 다 가능해요.",
+  imageUrl: "",
+  likeCount: 0,
+  ingredients: [
+    {
+      ingredient: "감자",
+    },
+    {
+      ingredient: "표고버섯",
+    },
+    {
+      ingredient: "양파",
+    },
+    {
+      ingredient: "칼국수 면",
+    },
+    {
+      ingredient: "다진마늘",
+    },
+    {
+      ingredient: "멸치액젓",
+    },
+    {
+      ingredient: "소금",
+    },
+    {
+      ingredient: "멸치액젓",
+    },
+    {
+      ingredient: "들깨가루",
+    },
+    {
+      ingredient: "들기름",
+    },
+  ],
+  steps: [
+    {
+      description:
+        "1. 감자는 0.5cm로 채 썰고, 표고버섯은 기둥을 제거한 후 0.3cm 두께로 편 썬다.",
+      imagePath: "",
+    },
+    {
+      description: "2. 양파는 0.3cm로 채 썬다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "3. 깊은 프라이팬이나 냄비에 물, 양파, 감자, 표고버섯을 넣고 끓으면 중불로 줄인다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "4. 칼국수 면을 체에 밭쳐 흐르는 물에 헹군 후 육수에 넣어 살살 저어가며 끓인다. * 칼국수 면을 미리 헹궈두면 뭉칠 수 있으니 육수에 넣기 직전에 헹군다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "5. 다시 끓어 오르면 간 마늘, 멸치액젓을 넣고 약 4분 30초 정도 끓인다. * 불 세기에 따라 물양을 조절한다. ",
+      imagePath: "",
+    },
+    {
+      description:
+        "6. 끓이는 중간에 간을 본 후 부족한 간은 꽃소금을 이용하여 맞춘다. * 다시다를 사용 할 경우 꽃소금은 생략 가능하다",
+      imagePath: "",
+    },
+    {
+      description:
+        "7. 칼국수 면이 익으면 들깻가루를 넣고 골고루 섞은 후 불을 끈다.",
+      imagePath: "",
+    },
+    {
+      description: "8. 들기름을 넣고 섞은 후 그릇에 담아 완성한다.",
+      imagePath: "",
+    },
+  ],
 
-    likeCount: 0,
-    comments: [
-    ],
-  };
+  likeCount: 0,
+  comments: [],
+};
 
-  let  porkKimchiJjim= {
-    id: 4,
-    name: "백종원 돼지김치찜",
-    description: "김치와 고기가 쭉~쭉 찢어지는 김치찜!",
-    imageUrl: "",
-    likeCount: 0,
-    ingredients: [
-      {
-        id: 207,
-        ingredient: "돼지앞다리살",
-        amount : "1kg"
-      },
-      {
-        id: 55,
-        ingredient: "신김치",
-        amount : "1.3kg"
-      },
-      {
-        id:32,
-        ingredient:"양파",
-        amount:"약 1/2개"
-      },
-      {
-        id:31,
-        ingredient:"대파",
-        amount:"1컵"
-      },
-      {
-        id:43,
-        ingredient:"청양고추",
-        amount:"3개"
-      },
-      {
-        id:123,
-        ingredient:"황설탕",
-        amount:"(황설탕)3큰술"
-      },
-      {
-        id:103,
-        ingredient:"간장",
-        amount:"2큰술"
-      },
-      {
-        id:102,
-        ingredient:"된장",
-        amount:"1큰술"
-      },
-      {
-        id:34,
-        ingredient:"다진마늘",
-        amount:"1큰술"
-      },
-      {
-        id:114,
-        ingredient:"고춧가루",
-        amount:"(굵은) 2큰술"
-      },
-    ],
-    steps: [
-      {
-        id: 1,
-        description: "1. 설탕, 국간장, 된장, 간 마늘, 고춧가루를 섞어 양념장을 만든다.",
-        imagePath: "",
-      },
-      {
-        id: 2,
-        description: "2. 양파는 굵게 채 썰고 대파는 송송 썬다.",
-        imagePath: "",
-      },
-      {
-        id: 3,
-        description: "3. 청양고추는 두껍게 썰고 고기는 주먹 크기로 자른다.",
-        imagePath: "",
-      },
-      {
-        id: 4,
-        description: "4. 냄비에 김치를 깔고 고기를 올린 후 다시 김치로 덮어준다.",
-        imagePath: "",
-      },
-      {
-        id: 5,
-        description: "5. 그 위에 양파, 청양고추, 대파를 넣는다. ",
-        imagePath: "",
-      },
-      {
-        id: 6,
-        description: "6. 물과 양념장을 잘 섞어 부어준다.",
-        imagePath: "",
-      },
-      {
-        id: 7,
-        description: "7. 센 불로 켜고 끓으면 중약불로 줄여 뚜껑 덮고 졸인다.",
-        imagePath: "",
-      },
-      {
-        id :8,
-        description:"8. 김치와 고기가 물러지면 간을 본 후 기호에 맞게 식초를 넣는다.",
-        imagePath: "",
-      },
-      {
-        id :9,
-        description:"9. 식초를 골고루 섞어 준 후 완성한다.",
-        imagePath: "",
-      }
-    ],
+let fishCakeRamen = {
+  name: "백종원 어묵듬뿍! 해물맛 라면",
+  description:
+    "제가 만든 마라소스가 출시되었습니다ㅎㅎ CU 가면 사실 수 있는데 ㅎㅎ백종원 마라소스와 어묵 듬뿍 넣은 라면이랑 먹으면 얼마나 맛있는지 몰라요 ㅎㅎ 사라는 건 아니고요 ㅎㅎ 그냥 그렇다고요 ㅎㅎ",
+  imageUrl: "",
+  likeCount: 0,
+  ingredients: [
+    {
+      ingredient: "라면사리",
+    },
+    {
+      ingredient: "어묵",
+    },
+    {
+      ingredient: "청양고추",
+    },
+    {
+      ingredient: "콩나물",
+    },
+    {
+      ingredient: "대파",
+    },
+    {
+      ingredient: "간장",
+    },
+    {
+      ingredient: "마라소스",
+    },
+  ],
+  steps: [
+    {
+      description:
+        "1. 냄비에 물(3과 1/2컵), 다시마, 건더기스프, 콩나물을 넣고 강 불에 끓인다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "2. 사각어묵은 반으로 잘라 두께 약 0.5cm 정도로 채 썰어 준비한다.",
+      imagePath: "",
+    },
+    {
+      description: "3. 청양고추, 대파는 두께 0.3cm로 송송 썰어 준비한다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "4. 육수가 끓어 오르면 준비해둔 어묵과 라면사리, 국간장을 넣고 끓인다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "5. 면이 전체적으로 한 가닥씩 풀어지면 마라소스를 넣어 잘 섞어가며 약 1분정도 끓인다. *마라소스가 없다면 굵은고추가루를 넣고 끓여도 좋다.",
+      imagePath: "",
+    },
+    {
+      description: "6. 청양고추, 대파를 넣고 섞어 완성한다.",
+      imagePath: "",
+    },
+  ],
 
-    likeCount: 0,
-    comments: [
-    ],
-  };
+  likeCount: 0,
+  comments: [],
+};
+
+let porkKimchiJjim = {
+  name: "백종원 돼지김치찜",
+  description: "김치와 고기가 쭉~쭉 찢어지는 김치찜!",
+  imageUrl: "",
+  likeCount: 0,
+  ingredients: [
+    {
+      ingredient: "돼지앞다리살",
+    },
+    {
+      ingredient: "신김치",
+    },
+    {
+      ingredient: "양파",
+    },
+    {
+      ingredient: "대파",
+    },
+    {
+      ingredient: "청양고추",
+    },
+    {
+      ingredient: "황설탕",
+    },
+    {
+      ingredient: "간장",
+    },
+    {
+      ingredient: "된장",
+    },
+    {
+      ingredient: "다진마늘",
+    },
+    {
+      ingredient: "고춧가루",
+    },
+  ],
+  steps: [
+    {
+      description:
+        "1. 설탕, 국간장, 된장, 간 마늘, 고춧가루를 섞어 양념장을 만든다.",
+      imagePath: "",
+    },
+    {
+      description: "2. 양파는 굵게 채 썰고 대파는 송송 썬다.",
+      imagePath: "",
+    },
+    {
+      description: "3. 청양고추는 두껍게 썰고 고기는 주먹 크기로 자른다.",
+      imagePath: "",
+    },
+    {
+      description: "4. 냄비에 김치를 깔고 고기를 올린 후 다시 김치로 덮어준다.",
+      imagePath: "",
+    },
+    {
+      description: "5. 그 위에 양파, 청양고추, 대파를 넣는다. ",
+      imagePath: "",
+    },
+    {
+      description: "6. 물과 양념장을 잘 섞어 부어준다.",
+      imagePath: "",
+    },
+    {
+      description: "7. 센 불로 켜고 끓으면 중약불로 줄여 뚜껑 덮고 졸인다.",
+      imagePath: "",
+    },
+    {
+      description:
+        "8. 김치와 고기가 물러지면 간을 본 후 기호에 맞게 식초를 넣는다.",
+      imagePath: "",
+    },
+    {
+      description: "9. 식초를 골고루 섞어 준 후 완성한다.",
+      imagePath: "",
+    },
+  ],
+
+  likeCount: 0,
+  comments: [],
+};
