d2344406bfc188b7db8584eca2439098a4452678
Kwon770 <sckwon770@gmail.com>
2021-07-23 08:30:09 +0900

    feat: Add sidebar design renewal

--- src/Home/Sidebar.js
+++ src/Home/Sidebar.js
@@ -2,13 +2,25 @@ import "./Sidebar.css";
 import React from "react";
 import ingredientData from "../data/ingredientData";
 import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/DeleteOutline";
+import Search from "@material-ui/icons/Search";
 import { useIngredientsId, useUnselectIngredient } from "../IngredientContext";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  myClassName: {
+    marginRight: "10px",
+    "&:hover": {
+      backgroundColor: "rgba(225, 49, 90, 0.1)",
+    },
+  },
+}));
 const Sidebar = () => {
   const refrigeratorIngredientsId = useIngredientsId();
   const unselectIngredient = useUnselectIngredient();
+
+  const classes = useStyles();
 
   console.log("임시 저장소 : " + refrigeratorIngredientsId);
   console.log("selected에 저장된 값 : " + refrigeratorIngredientsId);
@@ -16,32 +28,35 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <h1 className="sidebar-header">냉장고</h1>
+      <div style={{ width: "100%", height: "9px", background: "#235391" }} />
+      <div style={{ width: "100%", height: "7px", background: "#d4e5ff" }} />
+      <div style={{ width: "100%", height: "9px", background: "#235391" }} />
+
+      {/* <div style={{ width: "100%", height: "3px", background: "white" }} /> */}
       <div className="sidebar-content">
-        <ul className="ingredient-list">
+        <div className="ingredient-list">
           {refrigeratorIngredientsId.map((ingredientId) => {
             const name = ingredientData.getIngredientNameById(ingredientId);
             return (
-              <li key={ingredientId}>
-                <div className="ingredient-element">
-                  {name}
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    size="small"
-                    onClick={() => unselectIngredient(ingredientId)}
-                  >
-                    <DeleteIcon
-                      className="sidebar-delete-button"
-                      fontSize="medium"
-                    ></DeleteIcon>
-                  </Button>
-                </div>
-              </li>
+              <div className="ingredient-element" key={ingredientId}>
+                <img
+                  className="ingredient-pin"
+                  src={"/images/ingredient_pin.png"}
+                />
+                {name}
+                <IconButton
+                  className={classes.myClassName}
+                  onClick={() => unselectIngredient(ingredientId)}
+                >
+                  <DeleteIcon fontSize="medium" color="secondary" />
+                </IconButton>
+              </div>
             );
           })}
-        </ul>
+        </div>
         <Link to="/search" className="search-link">
-          레시피 보러 가기
+          <Search />
+          <div className="search-link-text">레시피 보러 가기</div>
         </Link>
       </div>
     </div>
