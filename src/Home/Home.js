c626218646ab70b714eaadbb1066518250337fd2
yunseong9999 <dbsdltjd3701@naver.com>
2021-07-25 16:32:00 +0900

    add ingredient Data(4)

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -31,7 +31,9 @@ const Home = () => {
           <HomeListSquare imageUrl={"/images/friedEggplant/main.jpg"} />
           <HomeListSquare imageUrl={"/images/lettuceGeotjeorl/main.jpg"} />
           <HomeListSquare imageUrl={"/images/eggJangjorim/main.jpg"} />
-          <HomeListSquare imageUrl={"/images/test/recipe5.jpg"} />
+          <HomeListSquare
+            imageUrl={"/images/marbledSoybeanPasteStew/main.jpg"}
+          />
           <HomeListSquare imageUrl={"/images/test/recipe6.jpg"} />
           <HomeListSquare imageUrl={"/images/test/recipe7.jpg"} />
         </div>
