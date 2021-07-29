10d4b535b32586987f3b3341b50555d75127d596
Kwon770 <sckwon770@gmail.com>
2021-07-29 14:23:55 +0900

    feat: Add responsive css in home page

--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header">
-      <Link exact to="/">
-        <span className="title">econo refrigerator</span>
+      <Link exact to="/" className="title">
+        <div>econo refrigerator</div>
       </Link>
 
-      <Link to="/recommendation">
-        <span className="menu">recommendation</span>
+      <Link to="/recommendation" className="menu">
+        <div>recommendation</div>
       </Link>
 
-      <Link to="/explore?ingredients=0">
-        <span className="menu">explore</span>
+      <Link to="/explore?ingredients=0" className="menu">
+        <div>explore</div>
       </Link>
     </div>
   );
